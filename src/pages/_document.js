3138d5c1798b7ac6c17286c8a2ee4889faa8f086
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-08-25 21:38:59 +0700

    add: icon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+      <link
+          rel='icon'
+          type='image/png'
+          href='/star.png'
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
