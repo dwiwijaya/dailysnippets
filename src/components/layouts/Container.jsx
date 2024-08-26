48e559af2d502289ea24139fd6972a077a29fb2f
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-08-26 23:05:09 +0700

    style: fix container 100vh to 100svh

--- src/components/layouts/Container.jsx
+++ src/components/layouts/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Container = ({ children }) => {
     return (
-        <div className="flex items-center justify-center h-screen sm:p-8">
+        <div className="flex items-center justify-center h-[100svh] sm:p-8">
             <div className="h-full w-full sm:w-[40rem] sm:h-[30rem] flex flex-col relative bg-container p-4 rounded-lg border border-stroke">
                 {children}
             </div>
