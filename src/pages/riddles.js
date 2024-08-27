4c5f240a18e7bc87ab64b509aac85288f569b0c4
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-08-27 18:29:48 +0700

    fix: riddler and trivia page

--- src/pages/riddles.js
+++ src/pages/riddles.js
@@ -3,6 +3,7 @@ import { dehydrate, QueryClient } from '@tanstack/react-query';
 import { useRefetch } from '@/contexts/RefetchContext';
 import { fetchNinjaAPI } from '@/libs/api';
 import StatusHandler from '@/components/StatusHandler';
+import { useEffect, useState } from 'react';
 
 const fetchRiddles = async () => {
   const response = await fetchNinjaAPI({ endpoint: 'riddles' });
@@ -11,29 +12,48 @@ const fetchRiddles = async () => {
 
 export default function RiddlePage() {
   const { refetchFlag } = useRefetch();
+  const [showAnswer, setShowAnswer] = useState(false);
 
   const { data, isLoading, isError } = useQuery({
     queryKey: ['riddles', refetchFlag],
     queryFn: fetchRiddles,
-    refetchOnWindowFocus: false, 
+    refetchOnWindowFocus: false,
   });
 
+  const toggleAnswer = () => {
+    setShowAnswer(!showAnswer);
+  };
+
+  useEffect(() => {
+    setShowAnswer(false)
+  }, [refetchFlag])
+
   return (
-    <div className="rounded-lg text-center ">
-      <h1 className="text-2xl font-bold mb-4 ">Today’s Riddle</h1>
+    <div className="rounded-lg text-center p-4">
+      <h1 className="text-2xl font-bold mb-4">Today’s Riddle</h1>
       <StatusHandler isLoading={isLoading} isError={isError} />
       {!isLoading && !isError && data && (
         <>
-          <p className="text-lg text-subtext font-medium">
+          <p className="text-lg text-subtext font-medium mb-4">
             {data[0].question}
           </p>
-          <p className="text-lg font-medium">
-            ~ {data[0].answer}
-          </p>
+          <button
+            onClick={toggleAnswer}
+            className="btn mb-4 w-full flex items-center justify-center space-x-2"
+          >
+            <i className={`fad ${showAnswer ? 'fa-eye-slash' : 'fa-eye'}`} />
+            <span>{showAnswer ? 'Hide Answer' : 'Show Answer'}</span>
+          </button>
+          {showAnswer && (
+            <p
+              className="text-lg font-medium transition-opacity duration-500 ease-in-out opacity-100"
+            >
+              {data[0].answer}
+            </p>
+          )}
         </>
       )}
     </div>
-
   );
 }
 
--- src/pages/trivia.js
+++ src/pages/trivia.js
@@ -3,6 +3,7 @@ import { dehydrate, QueryClient } from '@tanstack/react-query';
 import { useRefetch } from '@/contexts/RefetchContext';
 import { fetchNinjaAPI } from '@/libs/api';
 import StatusHandler from '@/components/StatusHandler';
+import { useEffect, useState } from 'react';
 
 const fetchTrivia = async () => {
   const response = await fetchNinjaAPI({ endpoint: 'trivia' });
@@ -10,13 +11,22 @@ const fetchTrivia = async () => {
 };
 
 export default function TriviaPage() {
+  const [showAnswer, setShowAnswer] = useState(false);
   const { refetchFlag } = useRefetch();
 
   const { data, isLoading, isError } = useQuery({
     queryKey: ['trivia', refetchFlag],
     queryFn: fetchTrivia,
-    refetchOnWindowFocus: false, 
+    refetchOnWindowFocus: false,
   });
+
+  const toggleAnswer = () => {
+    setShowAnswer(!showAnswer);
+  };
+
+  useEffect(() => {
+    setShowAnswer(false)
+  }, [refetchFlag])
 
   return (
     <div className="rounded-lg text-center ">
@@ -24,12 +34,23 @@ export default function TriviaPage() {
       <StatusHandler isLoading={isLoading} isError={isError} />
       {!isLoading && !isError && data && (
         <>
-          <p className="text-lg text-subtext font-medium">
+          <p className="text-lg text-subtext font-medium mb-4">
             {data[0].question}
           </p>
-          <p className="text-lg font-medium">
-            ~ {data[0].answer}
-          </p>
+          <button
+            onClick={toggleAnswer}
+            className="btn mb-4 w-full flex items-center justify-center space-x-2"
+          >
+            <i className={`fad ${showAnswer ? 'fa-eye-slash' : 'fa-eye'}`} />
+            <span>{showAnswer ? 'Hide Answer' : 'Show Answer'}</span>
+          </button>
+          {showAnswer && (
+            <p
+              className="text-lg font-medium transition-opacity duration-500 ease-in-out opacity-100"
+            >
+              {data[0].answer}
+            </p>
+          )}
         </>
       )}
     </div>
